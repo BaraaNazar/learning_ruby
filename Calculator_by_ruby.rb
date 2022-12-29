ab09a778a9692f9eaeb60cc422b093136566db77
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2022-12-29 21:09:22 +0300

    Completed Calculator App

---
+++ Calculator_by_ruby.rb
@@ -0,0 +1,16 @@
+puts 'Enter 1st Number:'
+number1 = gets.chomp.to_f
+puts 'Enter 2nd Number:'
+number2 = gets.chomp.to_f
+
+puts 'sum: '
+puts(number1 + number2)
+
+puts 'sub: '
+puts(number1 - number2)
+
+puts 'multiplying: '
+puts(number1 * number2)
+
+puts 'division: '
+puts(number1 / number2)
